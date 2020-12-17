47333008c648ff376a02f796ee52d7c47e12cc59
Mohammd-Ghazal <moh.g.ghazal@gmail.com>
2020-12-17 04:29:37 +0200

    q4 of DATA_STRUCTURE

--- 02_Data Structures + Algorithms [27 Q]/exercises/04_maxchar/index.js
+++ 02_Data Structures + Algorithms [27 Q]/exercises/04_maxchar/index.js
@@ -5,6 +5,22 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let max = -Infinity;
+  let maxChar;
+  const obj = {};
+
+  for (let char of str) {
+    obj[char] = obj[char] + 1 || 1;
+  }
+
+  for (key in obj) {
+    if (obj[key] > max) {
+      max = obj[key];
+      maxChar = key;
+    }
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
